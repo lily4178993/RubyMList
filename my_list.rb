b7c16853736a38081327d24af703271aad69f33f
lily4178993 <nellytelli@gmail.com>
2023-11-04 10:18:41 +0000

    feat(MyList): Add all?, any?, and filter methods

--- my_list.rb
+++ my_list.rb
@@ -12,3 +12,16 @@ class MyList
     @list.each(&block)
   end
 end
+
+list = MyList.new(1, 2, 3, 4)
+
+# Test #all?
+puts(list.all? { |e| e < 5 }) # Expected: true
+puts(list.all? { |e| e > 5 }) # Expected: false
+
+# Test #any?
+puts(list.any? { |e| e == 2 }) # Expected: true
+puts(list.any? { |e| e == 5 }) # Expected: false
+
+# Test #filter
+p(list.filter(&:even?)) # Expected: [2, 4]
