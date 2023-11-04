c13fe04dd21a92fba2a2261e88c19ca6cc7f2ee9
Ochiengsteven <stevenochieng6305@gmail.com>
2023-11-04 09:15:37 +0300

    ADD MY ENUMERABLE

---
+++ my_list.rb
@@ -0,0 +1,14 @@
+require_relative 'my_enumerable'
+
+class MyList
+  include MyEnumerable
+  attr_reader :list
+
+  def initialize(*args)
+    @list = args
+  end
+
+  def each(&block)
+    @list.each(&block)
+  end
+end
